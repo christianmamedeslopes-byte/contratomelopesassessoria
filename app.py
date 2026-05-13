5e1f2e915ccff4e50aefe5003213b500acf13af3
christianmamedeslopes-byte <christianmamedeslopes@gmail.com>
2026-05-13 17:07:47 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="M e Lopes Business Suite",
+    layout="wide"
+)
+
+st.title("🚀 M e Lopes Business Suite")
+
+st.success("Estrutura premium carregada com sucesso.")
